91ab7b99f16a644ab4263a7c1a612b53e3a2d4a1
Rafael Ribeiro <faelribeirodesousa@gmail.com>
2017-06-25 13:56:38 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Hello World</title>
+        <meta charset="utf-8">
+        <scritp src="js/jquery-3.2.1.min.js"></scritp>
+        <script src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <header>
+            <div class="page-header borda-titulo">
+                <h1>Clinica </h1>
+                <ul class="nav nav-pills">
+                    
+                </ul>
+            </div>
+        </header>
+    </body>
+</html>
